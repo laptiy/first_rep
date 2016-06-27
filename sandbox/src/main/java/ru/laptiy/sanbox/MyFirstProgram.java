a53f65cf158c43f35586975ace5d2d8fb8ae98a5
laptiy <laptiy@gmail.com>
2016-06-27 21:52:51 +0300

    добавлено енсолько функций

--- sandbox/src/main/java/ru/laptiy/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/laptiy/sanbox/MyFirstProgram.java
@@ -2,8 +2,27 @@ package ru.laptiy.sanbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+   public static void main(String[] args) {
+      hello("Me");
+      hello("You");
+      hello("We");
+      hello("Us");
 
+      double l = 5;
+      System.out.println("Площадь квадрата со стороной " + l + " равно " + area(l));
+      double a = 2;
+      double b = 3;
+      System.out.println("Площадь прямоугльника  со сторонами " + a + "и" + b + " равно " + area(a,b));
+   }
+
+   public static void hello(String smb) {
+      System.out.println("Hello " + smb + "!");
+   }
+
+   public static double area(double len) {
+      return len * len;
+   }
+   public static double area(double a, double b){
+      return a * b;
+   }
 }
